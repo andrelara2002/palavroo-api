5b87bbbe21ddec1d01c6d84ed3f5e89caa401795
andrelara2002 <andrelara2002@gmail.com>
2023-06-21 12:13:51 -0300

    Feat: Get random words function

---
+++ src/controller/WordController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+
+import { WordModel } from "../model/WordModel";
+
+import IWord from "../interfaces/WordInterface";
+
+function randomInRange(range: number): number {
+    return Math.floor(Math.random() * (range - 0) + 0)
+}
+
+async function randomWordBySizeAsync(lenght: number): Promise<IWord | undefined> {
+    const words = await WordModel.find({ lenght })
+
+    const index = randomInRange(words.length)
+
+    return words[index]
+}
+
+async function randomWordAsync(): Promise<IWord | null> {
+    const size = WordModel.length
+
+    const index = randomInRange(size)
+
+    const word = await WordModel.findOne().skip(index).exec()
+
+    return word
+}
+
+export async function getAllAsync(req: Request, res: Response) { }
+export async function getRandomAsync(req: Request, res: Response) { }
+export async function getRandomInRangeAsync(req: Request, res: Response, range: number) { }
+
+
