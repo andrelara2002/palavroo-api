070dbbf83ecc4ea9391a9ece40903355405fde1c
andrelara2002 <andrelara2002@gmail.com>
2023-06-21 18:25:36 -0300

    Test: Integrating tests

--- index.js
+++ index.js
@@ -39,23 +39,15 @@ connectToDatabaseAsync(MONGO_URI)
 
 module.exports = app */
 
-// index.js
 const express = require('express')
-
-const app = express()
-const PORT = 4000
+const app = express();
 
 app.listen(PORT, () => {
     console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-})
+app.get('/', (req, res) => res.send('Hello World'))
 
-app.get('/about', (req, res) => {
-    res.send('This is my about route..... ')
-})
 
-// Export the Express API
+
 module.exports = app
